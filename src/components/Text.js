5de61d146d154a30e615552fbe5bca944bb76fcb
chinthanah <chinthanahssuresh@gmail.com>
2023-09-04 14:33:12 +0530

    second commit

---
+++ src/components/Text.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export default function Text(props) {
+  const handleUpClick = () => {
+    console.log("Uppercase button clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    console.log("On change");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("enter text here");
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="exampleFormControlTextarea1"
+          rows="8"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
+    </div>
+  );
+}
